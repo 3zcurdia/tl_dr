eeff8fc7ea8f5b731ba56f3b33ff65335bef1177
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-09 11:16:09 -0500

    Initial commit

---
+++ lib/tl_dr.ex
@@ -0,0 +1,18 @@
+defmodule TlDr do
+  @moduledoc """
+  Documentation for `TlDr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TlDr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tl_dr_test.exs
@@ -0,0 +1,8 @@
+defmodule TlDrTest do
+  use ExUnit.Case
+  doctest TlDr
+
+  test "greets the world" do
+    assert TlDr.hello() == :world
+  end
+end
